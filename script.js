122f45c9a474a9538b59083913bb6c4c7d3777ac
zhizhizhi00 <zhizhix17@gmail.com>
2025-11-07 15:34:43 +0800

    met CSP task requirements

--- script.js
+++ script.js
@@ -8,40 +8,139 @@ alert('welcome ' + username + '!')
 alert('you are going to play rock paper scissors vs a computer')
 alert('if you win, you receive a cake, lose, receive coal!')
 
-choice = prompt('choose one: rock, paper, or scissors')
+document.getElementById('game').addEventListener('click',(e) => {
+    globalThis
+        choice = prompt('choose one: rock, paper, or scissors')
+    play_rps();
+})
+document.getElementById('rock').addEventListener('click',(e) => {
+    globalThis
+        choice = 'rock'
+    play_rps();
+})
+document.getElementById('paper').addEventListener('click',(e) => {
+    globalThis
+        choice = 'paper'
+    play_rps();
+})
+document.getElementById('scissors').addEventListener('click',(e) => {
+    globalThis
+        choice = 'scissors'
+    play_rps();
+})
 
-while (choice != 'rock' && choice != 'paper' && choice != 'scissors')
+function play_rps()
 {
-    alert('invalid choice!')
-    choice = prompt('choose one: rock, paper, or scissors')
+
+
+    while (choice != 'rock' && choice != 'paper' && choice != 'scissors')
+    {
+        alert('invalid choice!')
+        choice = prompt('choose one: rock, paper, or scissors')
+    }
+
+    computer_choices = ['rock', 'paper', 'scissors']
+    const randomIndex = Math.floor(Math.random() * computer_choices.length);
+    computer_choice = computer_choices[randomIndex]
+
+
+    let coal = document.createElement('img');
+    coal.src = './assets/coal.jpg';
+    coal.width = 200; 
+    coal.height = 200;
+
+
+    let cake = document.createElement('img');
+    cake.src = './assets/cake_image.jpg';
+    cake.width = 200;
+    cake.height = 200;
+
+
+    
+    //globalThis
+        function result(a)
+        {
+            alert(a + b)
+        }
+
+
+
+
+    if (computer_choice == choice)
+    {
+        a='draw!'
+        b=choice
+        result (a,b)
+        document.getElementById('rps').innerHTML="";
+        document.getElementById('rps').appendChild(coal)
+    }
+    else if (computer_choice == 'rock' && choice == 'paper')
+    {
+        a='win!'
+        b=choice
+        result (a,b)
+        document.getElementById('rps').innerHTML="";
+        document.getElementById('rps').appendChild(cake)
+        sound.play()
+    }
+    else if (computer_choice == 'rock' && choice == 'scissors')
+    {
+        a='lose!'
+        b=choice
+        result (a,b)
+        document.getElementById('rps').innerHTML="";
+        document.getElementById('rps').appendChild(coal)
+        sound.play()
+    }
+    else if (computer_choice == 'paper' && choice == 'rock')
+    {
+        a='lose!'
+        b=choice
+        result (a,b)
+        document.getElementById('rps').innerHTML="";
+        document.getElementById('rps').appendChild(coal)
+        sound.play()
+    }
+    else if (computer_choice == 'paper' && choice == 'scissors')
+    {
+        a='win!'
+        b=choice
+        result (a,b)
+        document.getElementById('rps').innerHTML="";
+        document.getElementById('rps').appendChild(cake)
+        sound.play()
+    }
+    else if (computer_choice == 'scissors' && choice == 'paper')
+    {
+        a='lose!'
+        b=choice
+        result (a,b)
+        document.getElementById('rps').innerHTML="";
+        document.getElementById('rps').appendChild(coal)
+        sound.play()
+    }
+    else if (computer_choice == 'scissors' && choice == 'rock')
+    {
+        a='win!'
+        b=choice
+        result (a,b)
+        document.getElementById('rps').innerHTML="";
+        document.getElementById('rps').appendChild(cake)
+        sound.play()
+    }
+    else
+        alert('ovo')
 }
-
-computer_choices = ['rock', 'paper', 'scissors']
-const randomIndex = Math.floor(Math.random() * computer_choices.length);
-computer_choice = computer_choices[randomIndex]
+     
 
 
-let coal = document.createElement('img');
-coal.src = './assets/coal.jpg';
-coal.width = 200; 
-coal.height = 200;
+       const sound = new Audio('./assets/boing.mp3') 
+        document.addEventListener('keydown', (e) => { 
+        if (e.key === 'g') 
+        sound.play(); 
+        }) 
 
 
-let cake = document.createElement('img');
-cake.src = './assets/cake_image.jpg';
-cake.width = 200;
-cake.height = 200;
-
-if (computer_choice == choice)
-{
-    alert('draw!')
-    document.getElementById('rps').appendChild(coal)
-}
-else
-{
-    alert('not draw!')
-    document.getElementById('rps').appendChild(cake)
-}
 
 
 
